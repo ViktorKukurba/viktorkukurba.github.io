64fc6fcab37e753038e7c95ada57813ce75a2272
Viktor Kukurba <victor.kukurba@sethq.com>
2017-03-05 13:03:53 +0200

    gh-pages update

--- send-email.php
+++ send-email.php
@@ -1,24 +1,20 @@
 <?php
-$filename = "emails";
+$filename = "emails.txt";
 try {
-//$format = "Author: %s (email: %s); Subject: %s; Message: %s";
-//$msg = sprintf($format, $_POST["name"], $_POST["email"], $_POST["subject"], $_POST["message"]);
-$request_body = file_get_contents('php://input');
-$data = json_decode($request_body, true);
-echo $data["json"];
-echo $data->json;
-echo $request_body;
-echo $HTTP_RAW_POST_DATA;
-file_put_contents($filename, $request_body . '/n' , FILE_APPEND);
-// send email "] . $_POST["email"] . $_POST["subject"] . $_POST["message"]
-mail("viktor.kukurba@gmail.com", "Message");
-//mail("viktor.kukurba@gmail.com", $msg);
-echo $_POST["email"];
-echo "done1";
-echo "done2";
+    $format = "Author: %s (email: %s); Subject: %s; Message: %s";
+    $request_body = file_get_contents('php://input');
+    $data = json_decode($request_body, true);
+    $msg = sprintf($format, $data["name"], $data["email"], $data["subject"], $data["message"]);
+    file_put_contents($filename, $msg . "/n" , FILE_APPEND);
+    mail("viktor.kukurba@gmail.com", "My site. " . $data["subject"], $msg);
+    header('Content-Type: application/json');
+    $response = new StdClass();
+    $response->success = true;
+    $response->data = $data;
+    echo json_encode($response);
 } catch(Exception $e) {
 //  mail("viktor.kukurba@gmail.com", "Exception");
-//file_put_contents($filename, $e->getMessage() , FILE_APPEND);
+    file_put_contents($filename, $e->getMessage(), FILE_APPEND);
 //  echo $e->getMessage();
 }
 ?>
